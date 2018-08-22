aec9e5c9e42f0a5878e42c6f09a72e10adf536e3
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-08-22 10:06:43 +0200

    correction

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 1</title>
-  </head>
-  <body>
-    <form class="" action="user.php" method="GET">
-      <input type="text" name="lastname" placeholder="Nom" />
-      <input type="text" name="firstname" placeholder="Prénom">
-      <input type="submit" value="Valider">
-    </form>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 1</title>
+    </head>
+    <body>
+        <form class="" action="user.php" method="GET">
+            <label for="lastname">nom</label>
+            <input id="lastname"type="text" name="lastname" placeholder="Nom" />
+            <label for="firstname">Prénom</label> <input id="fisrtname" type="text" name="firstname" placeholder="Prénom" />
+            <input type="submit" value="Valider">
+        </form>
 
-  </body>
+    </body>
 </html>
